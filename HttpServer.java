3a6f282e3fccee6eed0e3be64f50de5a9c0339c0
AlexeyTOP1 <atazhanov.biz@yandex.ru>
2018-12-14 13:21:09 +0700

    Simple http server, port 8080, + reader html files

---
+++ HttpServer.java
@@ -0,0 +1,84 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.Scanner;
+import java.io.*;
+import java.nio.file.*;
+
+
+/**
+ * Created by yar 09.09.2009
+ */
+public class HttpServer {
+
+    public static void main(String[] args) throws Throwable {
+        ServerSocket ss = new ServerSocket(8080);
+        while (true) {
+            Socket s = ss.accept();
+            System.err.println("Client accepted");
+            new Thread(new SocketProcessor(s)).start();
+        }
+    }
+
+    private static class SocketProcessor implements Runnable {
+
+        private Socket s;
+        private InputStream is;
+        private OutputStream os;
+
+        private SocketProcessor(Socket s) throws Throwable {
+            this.s = s;
+            this.is = s.getInputStream();
+            this.os = s.getOutputStream();
+        }
+
+        public void run() {
+            
+            
+            try {
+                
+               // FileReader fr = new FileReader("C:\\Users\\alexey\\Desktop\\http server java\\1.html");
+           // Scanner scan = new Scanner(fr);
+           FileReader fr = new FileReader("C:\\Users\\alexey\\Desktop\\http server java\\1.html");
+           Scanner scan = new Scanner(fr).useDelimiter("\\A");
+           // Scanner s = new Scanner(f).useDelimiter("\\A");
+                String result = scan.hasNext() ? scan.next() : "";
+                readInputHeaders();
+                writeResponse(result);
+            } catch (Throwable t) {
+                /*do nothing*/
+            } finally {
+                try {
+                    s.close();
+                } catch (Throwable t) {
+                    /*do nothing*/
+                }
+            }
+            System.err.println("Client processing finished");
+        }
+
+        private void writeResponse(String s) throws Throwable {
+            String response = "HTTP/1.1 200 OK\r\n" +
+                    "Server: YarServer/2009-09-09\r\n" +
+                    "Content-Type: text/html\r\n" +
+                    "Content-Length: " + s.length() + "\r\n" +
+                    "Connection: close\r\n\r\n";
+            String result = response + s;
+            os.write(result.getBytes());
+            os.flush();
+        }
+
+        private void readInputHeaders() throws Throwable {
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            while(true) {
+                String s = br.readLine();
+                if(s == null || s.trim().length() == 0) {
+                    break;
+                }
+            }
+        }
+    }
+}
