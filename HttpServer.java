6a137a413797259856e56163129a9717dffd9ced
AlexeyTOP1 <atazhanov.biz@yandex.ru>
2018-12-21 22:38:57 +0700

    upload files on request

--- HttpServer.java
+++ HttpServer.java
@@ -9,9 +9,7 @@ import java.io.*;
 import java.nio.file.*;
 
 
-/**
- * Created by yar 09.09.2009
- */
+
 public class HttpServer {
 
     public static void main(String[] args) throws Throwable {
@@ -42,12 +40,33 @@ public class HttpServer {
                 
                // FileReader fr = new FileReader("C:\\Users\\alexey\\Desktop\\http server java\\1.html");
            // Scanner scan = new Scanner(fr);
-           FileReader fr = new FileReader("C:\\Users\\alexey\\Desktop\\http server java\\1.html");
-           Scanner scan = new Scanner(fr).useDelimiter("\\A");
-           // Scanner s = new Scanner(f).useDelimiter("\\A");
-                String result = scan.hasNext() ? scan.next() : "";
-                readInputHeaders();
-                writeResponse(result);
+           
+               String InputString = readInputHeaders();
+              
+               String[] requestParam = InputString.split(" ");
+                String path = requestParam[1];
+                 System.out.println(path);
+                 System.out.println(path.equals("/"));
+                 if (path.equals("/")) 
+                  path = path + "index.html";
+                String[] pathSlash = path.split("/");
+                String p = pathSlash[1];
+                
+               
+                String PathP="";
+                if(!p.contains("."))
+                        PathP = p.concat(".html");
+                    else PathP = p;
+
+                File f = new File(PathP);
+                if (!f.exists()) {
+                    writeResponse("<html><head></head><body><h1>Error 404 file not found</h1></body></html>");
+               }else{
+                            FileReader fr = new FileReader(f);
+                            Scanner scan = new Scanner(fr).useDelimiter("\\A");
+                                String result = scan.hasNext() ? scan.next() : "";
+                            writeResponse(result);
+            }
             } catch (Throwable t) {
                 /*do nothing*/
             } finally {
@@ -62,7 +81,7 @@ public class HttpServer {
 
         private void writeResponse(String s) throws Throwable {
             String response = "HTTP/1.1 200 OK\r\n" +
-                    "Server: YarServer/2009-09-09\r\n" +
+                    "Server: MyServer\r\n" +
                     "Content-Type: text/html\r\n" +
                     "Content-Length: " + s.length() + "\r\n" +
                     "Connection: close\r\n\r\n";
@@ -71,14 +90,17 @@ public class HttpServer {
             os.flush();
         }
 
-        private void readInputHeaders() throws Throwable {
+        private String readInputHeaders() throws Throwable {
             BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String summ="";
             while(true) {
                 String s = br.readLine();
+                summ = summ.concat(s);
                 if(s == null || s.trim().length() == 0) {
                     break;
                 }
             }
+            return summ;
         }
     }
 }
